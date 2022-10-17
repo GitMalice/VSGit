f403a8873b311d6779f67e3e255485eefcf3c164
Malice <create@hagalaz.fr>
2022-10-17 17:49:35 +0200

    color switch

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.body.style.backgroundColor = 'red'
+setTimeout( () => {
+    document.body.style.backgroundColor = 'red'
+    setTimeout( () => {
+      document.body.style.backgroundColor = 'orange'
+      setTimeout( () => {
+        document.body.style.backgroundColor = 'blue'
+        setTimeout ( () => {
+          document.body.style.backgroundColor = 'purple'
+          setTimeout( () => {
+            document.body.style.backgroundColor = 'green'
+          },1000)
+        },1000)
+      }, 1000)
+    }, 1000)
+},1000)
