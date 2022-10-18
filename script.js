5762553c951ee7a6b67add1f39adef8e5c3ab8aa
Malice <create@hagalaz.fr>
2022-10-18 17:45:31 +0200

    function générique de changement de couleurs

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-setTimeout( () => {
-    document.body.style.backgroundColor = 'red'
-    setTimeout( () => {
-      document.body.style.backgroundColor = 'orange'
-      setTimeout( () => {
-        document.body.style.backgroundColor = 'blue'
-        setTimeout ( () => {
-          document.body.style.backgroundColor = 'purple'
-          setTimeout( () => {
-            document.body.style.backgroundColor = 'green'
-          },1000)
-        },1000)
-      }, 1000)
-    }, 1000)
-},1000)
+// setTimeout( () => {
+//     document.body.style.backgroundColor = 'red'
+//     setTimeout( () => {
+//       document.body.style.backgroundColor = 'orange'
+//       setTimeout( () => {
+//         document.body.style.backgroundColor = 'blue'
+//         setTimeout ( () => {
+//           document.body.style.backgroundColor = 'purple'
+//           setTimeout( () => {
+//             document.body.style.backgroundColor = 'green'
+//           },1000)
+//         },1000)
+//       }, 1000)
+//     }, 1000)
+// },1000)
+
+function colorSwitch(color, delay) {
+  setTimeout( () => {
+    document.body.style.backgroundColor = color
+  },1000)
+}
+
+colorSwitch('blue',1000)
+colorSwitch('red',2000)
+colorSwitch('green',3000)
